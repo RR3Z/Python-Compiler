bf1d09a4174ed26ae0e996f7c937b49577ff43dc
Egor <104233247+RR3Z@users.noreply.github.com>
2024-10-01 20:20:25 +0300

    Added multiline string literals tests

--- _tests/_start.bat
+++ _tests/_start.bat
@@ -1,6 +1,7 @@
 @echo off
 type input\comments_tests.txt | ..\x64\Debug\Python-Compiler.exe > comments_result.txt
 type input\string_literals_tests.txt | ..\x64\Debug\Python-Compiler.exe > string_literals_result.txt
+type input\multiline_string_literals_tests.txt | ..\x64\Debug\Python-Compiler.exe > multiline_string_literals_result.txt
 type input\numeric_literals_tests.txt | ..\x64\Debug\Python-Compiler.exe > numeric_literals_result.txt
 type input\delimiters_tests.txt | ..\x64\Debug\Python-Compiler.exe > delimiters_result.txt
 type input\keywords_tests.txt | ..\x64\Debug\Python-Compiler.exe > keywords_result.txt
