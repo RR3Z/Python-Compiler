adb71c3187c9c8d7a88639c5cf4f01539093e9a8
Egor <104233247+RR3Z@users.noreply.github.com>
2024-11-24 22:21:39 +0300

    refactor

--- Python-Compiler/nodes/nodes.h
+++ Python-Compiler/nodes/nodes.h
@@ -10,91 +10,106 @@ struct IdentifierListNode;
 /* ========== EXPRESSION ========== */
 
 struct ExprNode {
-	// ������ ����
+	// Индекс узла
 	int id = -1;
-	// ��� ����
+	// Тип узла
 	NodeType exprType = _UNKNOWN;
 
-	// ������������� - �������� ����������, �������, ������ � �.�.
+	// Название переменной
 	string identifier = "";
 
-	// ��� �������� �������� ��������
+	// Значения
 	int boolVal = -1;
 	float floatVal = -1.0f;
 	int intVal = -1;
 	string stringVal = "";
 
-	// ��� ��������� expr
+	// Для бинарного expr
 	ExprNode* left = nullptr;
 	ExprNode* right = nullptr;
 
-	// ��������� expr � ������ exprList (������������ ������ ��� ������ � exprList)
+	// ТОЛЬКО ДЛЯ EXPRLIST
 	ExprNode* next = nullptr;
 
-	// ������������ ���: ������ ��������� list (��� ��� ��������)
+	// ТОЛЬКО ДЛЯ ЗАДАНИЯ LIST - [1,2,3]
 	ExprListNode* list = nullptr;
 
-	// ������������� ���: �������� �������� ��� slicing
+	// ТОЛЬКО ДЛЯ ЗАДАНИЯ LIST СО SLICING
 	SlicingNode* slicing = nullptr;
-
-	// ������������ ���: ������ ��������� list (��� ��� ��������)
-	IdentifierListNode* listId = nullptr;
 };
 
 /* ========== EXPRESSION LIST ========== */
 
 struct ExprListNode {
-	// ������ ����
+	// Индекс узла
 	int id = -1;
 
-	// ������ ������� ������
+	// Первый элемент списка
 	ExprNode* first = nullptr;
-	// ��������� ������� ������
+	// Последний элемент списка
 	ExprNode* last = nullptr;
 };
 
 /* ========== SLICING ========== */
 
 struct SlicingNode {
-	// ������ ����
+	// Индекс узла
 	int id = -1;
 
-	// ������ �������, ������� ����������
+	// С какого значения (включительно)
 	ExprNode* start = nullptr;
-	// ������� ����� �������� �������������
+	// До какого значения (не включительно)
 	ExprNode* end = nullptr;
-	// ���, � ������� ���������� ��������
+	// Шаг
 	ExprNode* step = nullptr;
 };
 
 /* ========== IDENTIFIERS ========== */
 
 struct IdentifierListNode {
-	// ������ ����
+	// Индекс узла
 	int id = -1;
 
-	// ������ ������� ������
-	ExprNode* firstId = nullptr;
-	// ��������� ������� ������
-	ExprNode* lastId = nullptr;
+	// Первый элемент списка
+	ExprNode* first = nullptr;
+	// Последний элемент списка
+	ExprNode* last = nullptr;
 };
+
 /* ========== TARGET ========== */
 
 struct TargetNode {
-	// ������ ����
+	// Индекс узла
 	int id = -1;
-	// ��� ����
+	// Тип узла
 	NodeType targetType = _UNKNOWN;
 
-	// ������������� - �������� ����������, �������, ������, ���������, ������ � �.�.
+	// Идентификатор - название переменной, функции, метода, аттрибута, класса и т.д.
 	string identifier = "";
 
-	// ��� ��������� target
+	// Для бинарного target
 	ExprNode* left = nullptr;
 	ExprNode* right_expr = nullptr;
 	TargetNode* right_target = nullptr;
 
-	// ������������� ���: �������� �������� ��� slicing
+	// Используяется для: указания значений для slicing
 	SlicingNode* slicing = nullptr;
+
+	// Следующий expr в списке targetList (используется только при работе с targetList)
+	TargetNode* next = nullptr;
+
+	// Используется для: списка элементов list (при его создании)
+	TargetListNode* list = nullptr;
 };
 
+/* ========== TARGET LIST ========== */
+
+struct TargetListNode {
+	// Индекс узла
+	int id = -1;
+
+	// Первый элемент списка
+	TargetNode* first = nullptr;
+	// Последний элемент списка
+	TargetNode* last = nullptr;
+};
