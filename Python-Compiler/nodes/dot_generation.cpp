25f8c569c12363d9edb391675c70337e7358c566
Egor <104233247+RR3Z@users.noreply.github.com>
2024-11-24 22:23:36 +0300

    Added dot generation for Identifiers

--- Python-Compiler/nodes/dot_generation.cpp
+++ Python-Compiler/nodes/dot_generation.cpp
@@ -215,6 +215,27 @@ string generateDotFromExprListNode(ExprListNode* node) {
 	return dot;
 }
 
+string generateDotFromIdentifierListNode(IdentifierListNode* node) {
+	string dot = "";
+
+	if (node == nullptr) { return dot; }
+
+	if (node->first != nullptr) {
+		ExprNode* expr = node->first;
+
+		dot += generateDotFromExprNode(expr);
+		dot += dotConnection(node->id, expr->id);
+
+		while (expr->next != nullptr) {
+			dot += generateDotFromExprNode(expr->next);
+			dot += dotConnection(node->id, expr->next->id);
+			expr = expr->next;
+		}
+	}
+
+	return dot;
+}
+
 string generateDotFromSlicingNode(SlicingNode* node) {
 	string dot = "";
 
--- Python-Compiler/nodes/dot_generation.h
+++ Python-Compiler/nodes/dot_generation.h
@@ -5,6 +5,8 @@ string generateDotFromExprNode(ExprNode* node);
 
 string generateDotFromExprListNode(ExprListNode* node);
 
+string generateDotFromIdentifierListNode(IdentifierListNode* node);
+
 string generateDotFromSlicingNode(SlicingNode* node);
 
 string generateDotFromTargetNode(TargetNode* node);
