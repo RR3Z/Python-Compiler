728a5a4b6e453a2ea65c838032e2f8b70cbb1285
Egor <104233247+RR3Z@users.noreply.github.com>
2025-01-20 14:13:53 +0300

    Поправил логику NOT в __BASE__

--- Python-Compiler/__BASE__.java
+++ Python-Compiler/__BASE__.java
@@ -251,7 +251,8 @@ public class __BASE__ {
 
     public __BASE__ __not_eql__(__BASE__ o) {
         try {
-            return new __BASE__(__eql__(o).__bVal);
+            if(__eql__(o).__bVal == true) return new __BASE__(false);
+            return new __BASE__(true);
         } catch(UnsupportedOperationException e) {
             throw new UnsupportedOperationException("not_eql isn't support operation for types: " + this.__type + " and: " + o.__type);
         }
